7a3973aa015492132f064ef8a4541779075587d6
Miguelangel <silvermiguel96@gmail.com>
2019-07-12 09:49:18 -0500

    Argument

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
 const express = require('express')
 const gqlmiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -10,11 +10,9 @@ const resolvers = require('./lib/resolvers')
 const app = express()
 const port = process.env.port || 3000
 
-const schema = buildSchema(
-  readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8')
-)
+const typeDefs =  readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),'utf-8')
+const schema = makeExecutableSchema({ typeDefs, resolvers})
 
 app.use('/api', gqlmiddleware({
   schema: schema,
